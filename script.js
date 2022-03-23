3958206be8994ff76029b51d20e7181ec6228965
ROGU3N1NJA <smeershoek2006@gmail.com>
2022-03-23 08:06:47 +0100

    fixed the adding lines

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ function modalOpenClose() {
 //
 
 function addLines() {
+  const main = document.querySelector("main");
+  var vw = main.clientWidth;
+  var vh = main.clientHeight;
   const divs = document.querySelectorAll("main > button");
   const lineContainer = document.querySelector(".lines-container");
 
@@ -29,15 +32,30 @@ function addLines() {
     } else {
       target = divs[index + 1];
     }
-    const box = el.getBoundingClientRect();
-    const box2 = target.getBoundingClientRect();
+
+    let parentPos = main.getBoundingClientRect();
+    let childPos = el.getBoundingClientRect();
+    let relativePos = {};
+    relativePos.top = childPos.top - parentPos.top;
+    relativePos.right = childPos.right - parentPos.right;
+    relativePos.bottom = childPos.bottom - parentPos.bottom;
+    relativePos.left = childPos.left - parentPos.left;
+    const box = relativePos;
+
+    let childPosTarget = target.getBoundingClientRect();
+    let relativePosTarget = {};
+    relativePosTarget.top = childPosTarget.top - parentPos.top;
+    relativePosTarget.right = childPosTarget.right - parentPos.right;
+    relativePosTarget.bottom = childPosTarget.bottom - parentPos.bottom;
+    relativePosTarget.left = childPosTarget.left - parentPos.left;
+    const box2 = relativePosTarget;
 
     let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line.setAttribute("x1", box.left + el.clientWidth / 2);
-    line.setAttribute("y1", box.top + el.clientHeight / 2);
-    line.setAttribute("x2", box2.left + el.clientWidth / 2);
-    line.setAttribute("y2", box2.top + el.clientHeight / 2);
+    line.setAttribute("x1", `${((box.left + el.clientWidth / 2) / vw) * 100}%`);
+    line.setAttribute("y1", `${((box.top + el.clientHeight / 2) / vh) * 100}%`);
+    line.setAttribute("x2", `${((box2.left + target.clientWidth / 2) / vw) * 100}%`);
+    line.setAttribute("y2", `${((box2.top + target.clientHeight / 2) / vh) * 100}%`);
     line.setAttribute("stroke", "black");
     svg.append(line);
     svg.classList.add(`line`);
